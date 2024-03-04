2aa29a0b7ac48762ca5301402eb18b17cb0fa5c1
LordLays <Rafal.Wasiluk2@gmail.com>
2024-03-04 14:15:21 +0100

    Rework Trip Model

--- Models/TripModel.cs
+++ Models/TripModel.cs
@@ -2,5 +2,11 @@
 {
     public class TripModel
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public string Place { get; set; }
+        public DateTime Date { get; set; }
+        public TimeSpan Duration { get; set; }
     }
 }
