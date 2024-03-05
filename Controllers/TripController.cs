d9b46d083b06ebffb3e3997d5e8a471a908b0388
LordLays <Rafal.Wasiluk2@gmail.com>
2024-03-05 11:29:01 +0100

    Create Controler for Trip

---
+++ Controllers/TripController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class TripController : Controller
+    {
+        private readonly ILogger<TripController> _logger;
+        private readonly IList<TripModel> _tripsList = TripModel.GetAllTrips();
+        // GET: TripController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: TripController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: TripController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TripController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TripController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: TripController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TripController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: TripController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
