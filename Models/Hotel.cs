b77845a5ed7ddf85ed23d217a4c7a118a96fa2fd
LordLays <Rafal.Wasiluk2@gmail.com>
2024-03-29 14:41:31 +0100

    Comint unsave file

--- Models/Hotel.cs
+++ Models/Hotel.cs
@@ -10,7 +10,18 @@
         public int StarRating { get; set; }
         public List<HotelRoom> Rooms { get; set; }
         public string? Description { get; set; }
+        public LinkedList<Reviews> Reviews { get; set; }
         public double AverageCustomersRating { get; set; }
         public List<string>? ImageUrl { get; set; }
+
+        public double AverageRating()
+        {
+            double sum = 0;
+            foreach (var review in Reviews)
+            {
+                sum += review.Rating;
+            }
+            return sum / Reviews.Count;
+        }
     }
 }
--- Models/Reviews.cs
+++ Models/Reviews.cs
@@ -2,5 +2,9 @@
 {
     public class Reviews
     {
+        public uint ID { get; set; }
+        public int Rating { get; set; }
+        public string? Review { get; set; }
+        public Custormers Customer { get; set; }
     }
 }
