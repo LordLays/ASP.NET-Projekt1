109a633739966cc52ffb4405974d67040bb9fd3b
LordLays <Rafal.Wasiluk2@gmail.com>
2024-03-29 14:37:38 +0100

    Model Change

---
+++ Models/HotelRoom.cs
@@ -0,0 +1,22 @@
+﻿namespace WebApplication1.Models
+{
+    public enum Type
+    {
+        Single,
+        Double,
+        Twin,
+        Family,
+        Suite,
+        Studio
+        
+    }
+    public class HotelRoom
+    {
+        public uint ID { get; set; }
+        public Type Type { get; set; }
+        public string? Description { get; set; }
+        public uint Capacity { get; set; }
+        public decimal Price { get; set; }
+        public bool Available { get; set; }
+    }
+}
